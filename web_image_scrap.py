76021d1933825613a133126951e2a12095d917b4
takuya yamaguchi padilla <takuya@vlan-2678-10-17-67-67.staff.wireless.sydney.edu.au>
2017-06-13 14:20:16 +1000

    added web image scrapper

---
+++ web_image_scrap.py
@@ -0,0 +1,19 @@
+# Download images from a site
+# In this case from someone S3 bucket
+import urllib.request
+
+count = 0
+for i in range(0, 4000):
+    f = str(i).zfill(4)
+    img = "https://xya.s3.amazonaws.com/media/fotos/XXX_{}.jpg".format(f)
+    try:
+        if urllib.request.urlopen(img).code == 200:
+            urllib.request.urlretrieve(
+                img, "/Users/yyy/Desktop/img/{}.jpg".format(f))
+            count += 1
+    except:
+        pass
+
+print("-----------------------------------------")
+print("   FINISH -> Downloaded {} elements".format(count))
+print("-----------------------------------------")
